70e46cad2c2d572248da6971766709ab1bcf4f15
Liam <liammc12@hotmail.co.uk>
2024-09-06 16:57:55 +0100

    link to discord and refactor

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 import puppeteer from 'puppeteer-extra';
+import pluginStealth from 'puppeteer-extra-plugin-stealth'
 
+import dotenv from 'dotenv'; 
+import { Webhook } from 'discord-webhook-node';
 
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-puppeteer.use(StealthPlugin())
+// Configure dotenv to load environment variables
+dotenv.config();
+puppeteer.use(pluginStealth())
+const hook = new Webhook(process.env.DISCORD);
 
 // Function to create a random delay between 1 and 5 seconds
 const delay = Math.floor(Math.random() * 5) + 1;
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-// const targetEvent = "https://www.twickets.live/en/event/1828748567179698176#sort=FirstListed&typeFilter=Any&qFilter=All";
-const targetEvent = "https://www.twickets.live/en/event/1796529798533615616#sort=FirstListed&typeFilter=Any&qFilter=All";
+const targetEventURL = "https://www.twickets.live/en/event/1828748567179698176#sort=FirstListed&typeFilter=Any&qFilter=All";
+// const targetEventURL = "https://www.twickets.live/en/event/1796529798533615616#sort=FirstListed&typeFilter=Any&qFilter=All";
 
-const sendMessage = async (price) => {
-  console.log(`Sending a message and price ${price}!`)
+const updateDiscord = async (page, message, file) => {
+  try {
+    await page.screenshot({ path: "./screenshots/" + file }, { fullPage: true });
+    console.log(message)
+    hook.send(message);
+    hook.sendFile("./screenshots/" + file);
+  } catch (e) {
+    console.log(e)
+    process.exit(0)
+  }
 }
-
 
 // Asynchronous main function to run the bot
 const bottus = async () => {
@@ -24,15 +36,22 @@ const bottus = async () => {
   // Function to check for tickets
   const checkTickets = async () => {
     const browser = await puppeteer.launch({
+      ignoreHTTPSErrors: true,
       headless: false, // Set to false if you want to see the browser
-      defaultViewport: null,
     });
 
     const page = await browser.newPage();
+    await page.emulate({
+      name: "Desktop 1024x1200",
+      viewport: {
+        width: 1024,
+        height: 1200,
+      },
+    });
 
     try {
       await page.goto(
-        targetEvent,
+        targetEventURL,
         {
           waitUntil: "domcontentloaded",
         }
@@ -47,18 +66,21 @@ const bottus = async () => {
 
       // Wait for the selector '.buy-button' to appear, with a 5-second timeout
       await page.waitForSelector(".buy-button", { timeout: 5000 });
+      const buyButtonText = await page.$eval('.buy-button', el => el.innerText);
+      console.log(buyButtonText, 'Buy Button Text')
 
       // If the selector is found, tickets are available
-      console.log("Buy button found! Tickets are available. Alerting Bottus!");
+      if (buyButtonText === "BUY") {
+        const priceString = await page.$eval('.no-of-ticket-summary', el => el.innerText);
+        // const priceMatch = priceString.match(/[\d]+[.,]\d{2}/);
 
-      await page.screenshot({path: "tickets/tickets-available.png"})
+        // Extract the price if found
+        // const price = priceMatch ? priceMatch[0] : null;
+        console.log("Buy button found! Tickets are available. Alerting Bottus!");
+        // await page.screenshot({path: "tickets/available.png"})
 
-      const price = await page.evaluate(() => {
-        return document.querySelectorAll('.no-of-ticket-summary strong')[1].innerText;
-      })
-      sendMessage(price);
-      
-
+        await updateDiscord(page, `TICKETS AVAILABLE! ${priceString}`, 'available.png');
+      }        
     } catch (error) {
       // Handle errors, especially TimeoutError when the selector is not found
       if (error.name === "TimeoutError") {
@@ -71,7 +93,6 @@ const bottus = async () => {
       await browser.close();
     }
   };
-
   // Execute the checkTickets function
   await checkTickets();
 };
